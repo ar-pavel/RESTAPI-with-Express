759cca38dded6ce6cc9fc50266b3a70618347f3d
ar-pavel <atikur@cefalo.com>
2021-09-14 14:57:43 +0600

    app restructured

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello Express!");
+const express = require('express')
+const routes = require('./routes/Routes')
+
+const app = express();
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+
+app.use(routes)
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, ()=> console.log("SERVER IS RUNNING AT PORT:", PORT));
+
+
