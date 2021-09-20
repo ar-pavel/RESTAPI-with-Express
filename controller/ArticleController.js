39ccefcf37ebe442ec17461ada3b9b05e2c8cfd6
ar-pavel <atikur@cefalo.com>
2021-09-20 15:34:22 +0600

    search query parameter updated from title to id

--- controller/ArticleController.js
+++ controller/ArticleController.js
@@ -53,6 +53,46 @@ const ArticleController = (Article) => {
         }
     }
     
+    async function getArticleByID(req, res) { 
+        try{
+            console.log("Request params: ",req.params);
+            const data = await Article.findByID(req.params.id);
+
+            if(!data){
+                return res.status(204).send("No content")
+            }
+
+            console.log("Searched data:", data);
+            
+            
+            res.status(200);
+            
+            res.format({
+                html: function (){
+                    res.send(FORMAT(data).html());
+                }, 
+                csv: function (){
+                    res.send(FORMAT(data).csv());
+                },
+                xml: function (){
+                    res.send(FORMAT(data).xml());
+                },
+                text: function (){
+                    res.send(FORMAT(data).text());
+                },
+                json: function(){
+                    res.json(data);
+                },
+            });
+            
+        }catch(err){
+            // request can't be processed
+            console.log(err);
+            res.status(500);
+            return res.send("Internal Server Error");
+        }
+    }     
+    
     async function getArticleByTitle(req, res) { 
         try{
             const data = await Article.findByTitle(req.params.title);
@@ -92,26 +132,32 @@ const ArticleController = (Article) => {
         }
     } 
     
-    async function updateArticleByTitle(req, res) { 
+    async function updateArticleByID(req, res) { 
         try{
               // create article
-            const article = {
+            const updatedArticle = {
+                id: req.param.id,
                 title: req.body.title,
                 description: req.body.description,
                 author: req.body.author,
             };
 
-            const tmp = await Article.findByTitle(req.params.title);
-            console.log("Data found:", tmp);
+            console.log("Request params: ",req.params);
 
-            console.log(article.author, req.user.username)
 
-            if(article.author != req.user.username){
+            const oldArticle = await Article.findByID(req.params.id);
+            console.log("Data found:", oldArticle);
+
+
+            console.log(oldArticle.author, req.user.username)
+
+
+            if(oldArticle.author != req.user.username){
                 return res.status(401).send({message : "Not Authorized"});
             }
 
 
-            const data = await Article.updateByTitle(req.params.title, article);
+            const data = await Article.updateByID(req.params.id, updatedArticle);
             
             res.status(200);
             
@@ -140,7 +186,31 @@ const ArticleController = (Article) => {
             
             return res.status(500).send({message:"Internal Server Error"});
         }
-    }
+    }   
+    
+    async function deleteArticleByID(req, res) { 
+        try{
+            
+            const article = await Article.findByID(req.params.id);
+            console.log(article);
+
+            console.log(article.author, req.user.username)
+
+            if(article.author != req.user.username){
+                return res.status(401).send({message : "Not Authorized"});
+            }
+
+            const data = await Article.deleteArticleByID(req.params.id);
+            
+            
+            return res.status(200).send(data);
+            
+        }catch(err){
+            // request can't be processed
+            console.log(err);
+            return res.status(500).send({message: "Internal Server Error"});
+        }
+    } 
     
     async function deleteArticleByTitle(req, res) { 
         try{
@@ -194,9 +264,10 @@ const ArticleController = (Article) => {
         
         console.log("requested data:", article);
         
-        // save to database 
         try{
+
             const data  = await Article.create(article);
+
             if(data){
                 // return res.status(201).send(data);
                 res.status(201);
@@ -217,7 +288,8 @@ const ArticleController = (Article) => {
                         res.json(data);
                     },
                 });
-            } 
+            }
+
             return res.status(500).send({
                 message:
                 err.message || "Some error occurred while creating the Article."
@@ -232,7 +304,7 @@ const ArticleController = (Article) => {
         
     }
     
-    return {getArticles, createArticle, getArticleByTitle, deleteArticleByTitle, deleteArticles, updateArticleByTitle};
+    return {getArticles, createArticle, getArticleByTitle, getArticleByID, deleteArticleByTitle, deleteArticleByID, deleteArticles, updateArticleByID};
 };
 
 module.exports = ArticleController
